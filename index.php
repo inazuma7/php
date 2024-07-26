c234d486aa8e690bc1b0be783e5b49415ce97cea
Inazuma <96759468+inazuma7@users.noreply.github.com>
2024-07-26 22:52:23 +0800

    yayyyyy

--- index.php
+++ index.php
@@ -8,10 +8,32 @@
 <body>
 
 <?php
+   // variable using $
    $name = "Nur Aleina";
    echo "hello $name";
-?>
 
+   /*scalartype
+   $string = "Aleina";
+   $int = 123456;
+   $float = 2.333;
+   $bool = true;
+
+   arraytype
+   $names = ["Aleina", "Sunday", "Monday"];
+
+   scalartype - if contain one type
+   $string = "";
+   $int = 0;
+   $float = 0;
+   $bool = false;
+
+   $array = [];
+   $object = null
+
+   object type
+   $object = new Car();*/
+?>
+ <p>Hello! My name is <?php echo$name ?> Hello world!</p>
 
 </body>
 </html>
