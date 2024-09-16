947112d661bdf90a849784b9ee36fca67fbb2b10
Inazuma <96759468+inazuma7@users.noreply.github.com>
2024-09-16 14:21:11 +0800

    Add files via upload

---
+++ operator.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+  
+/*String Operator
+
+$a = "Hello";
+$b = "World!";
+$c = $a ." ". $b;
+echo $c;*/
+
+/*arithmetic Operator
+echo (1+2)*4;*/
+
+/*Assigment operator
+$a = 2;
+$a += 4;
+echo $a;*/
+
+/*Comparasion operator
+$a = 2;
+$b = 4;
+ if ($a != $b){
+    echo "This statement is true!";
+ }*/
+
+/*logical operator
+$a = 4;
+$b = 4;
+
+$c = 2;
+$d = 6;
+ if ($a == $b or $c== $d) {
+    echo "This statement is true!";
+ }*/
+
+//incrementing/decrementing operator
+$a = 1;
+echo ++$a;
+
+
+$b = 1;
+echo --$b;
+
+$c =1;
+echo $c++;
+echo $c;
+
+$d = 1;
+echo $d--;
+echo $d;
+
+?>
+</body>
+</html>
